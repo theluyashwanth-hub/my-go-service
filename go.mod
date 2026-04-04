b086f7e2e8aa48577631cd5bc6515a8324822179
theluyashwanth-hub <theluyashwanth@gmail.com>
2026-04-04 18:04:27 +0000

    Initial Go app setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/theluyashwanth-hub/my-go-service
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+func main() {
+    http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "OK")
+    })
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    http.ListenAndServe(":"+port, nil)
+}
