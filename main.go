ceaad932633b93f876722c7824f2322799d33aa7
theluyashwanth-hub <theluyashwanth@gmail.com>
2026-04-04 20:44:27 +0000

    updated health message to version v3

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
     http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "OK")
+        fmt.Fprintln(w, "OK-v3 is running")
     })
     port := os.Getenv("PORT")
     if port == "" {
